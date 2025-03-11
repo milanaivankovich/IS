96d9e986ecfa8d08291529b688466623c3d15915
sunci0089 <unav660@gmail.com>
2025-03-11 12:22:08 +0100

    paginacija notifikacija frontend
    npm install react-intersection-observer

--- frontend/src/components/NotificationPanel.js
+++ frontend/src/components/NotificationPanel.js
@@ -2,18 +2,23 @@ import NotificationCard from "./NotificationCard";
 import API from "../variables.js"
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { ToastContainer } from "react-bootstrap";
+import { Spinner, ToastContainer } from "react-bootstrap";
 import userImg from "../images/user.svg";
+import { useInView } from "react-intersection-observer";
 
 const NotificationPanel = ({ userId }) => {
 
+    const [nextPage, setNextPage] = useState(null);
+    //dohvacanje 10 notifikacija
     const [notifications, setNotifications] = useState([]);
     const [loadingNotifications, setLoadingNotifications] = useState(true);
     const fetchAll = async () => {
         setLoadingNotifications(true);
-        await axios.get(`${API}/api/notifications/${userId}`) //token todo
-            .then((response) =>
-                setNotifications(response.data))
+        await axios.get(`${API}/api/notifications/pagination/${userId}`) //token todo
+            .then((response) => {
+                setNotifications(response.data.results)
+                setNextPage(response.data?.next ? response.data.next : null);
+            })
             .catch((error) =>
                 console.error("Greska pri dohvacanju notifikacija"))
             .finally(() =>
@@ -23,17 +28,39 @@ const NotificationPanel = ({ userId }) => {
         fetchAll();
     }, []);
 
+    //inView paginacija
+    const [isFetchingNextPage, setIsFetchingNextPage] = useState(false);
+    const fetchNextPage = async () => {
+        setIsFetchingNextPage(true);
+        await axios.get(`${nextPage}`) //token todo
+            .then((response) => {
+                setNotifications((prev) => Array.isArray(prev) ? [...prev, ...response.data.results] : response.data.results);
+                setNextPage(response.data?.next ? response.data.next : null);
+            })
+            .catch((error) =>
+                console.error("Greska pri dohvacanju notifikacija"))
+            .finally(() =>
+                setIsFetchingNextPage(false));
+    };
+
+    const { ref } = useInView({
+        threshold: 1,
+        onChange: (inView) => {
+            if (inView && nextPage) fetchNextPage();
+        }
+    });
+
     return (
         <>
             <div className="Event-bar-title">NOTIFIKACIJE</div>
             {loadingNotifications ? (
-                <p>Učitavanje...</p>
+                <Spinner className='spinner-border' animation="border" />
             ) : notifications.length === 0 ? (
                 <p>Nema notifikacija.</p>
-            ) : (
+            ) : (<>
                 <ToastContainer className="position-static">
 
-                    {notifications.map((notif) => (
+                    {notifications?.map((notif) => (
                         <NotificationCard key={notif.id}
                             userImgLink={notif.sender.profile_picture ? `${API}${notif.sender.prfile_picture}` : userImg}
                             userId={userId}
@@ -43,6 +70,11 @@ const NotificationPanel = ({ userId }) => {
                     }
 
                 </ToastContainer>
+
+                <div ref={ref} style={{ height: "20px" }}>
+                    {isFetchingNextPage && <Spinner className='spinner-border' animation="border" />}
+                </div>
+            </>
             )}
 
         </>);
