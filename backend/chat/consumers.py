b8045f2dce4e55a5d2f63317a66a5ad3684cea61
anastasijamilenic <milenicanastasija@gmail.com>
2025-03-22 17:25:25 +0100

    Izmjene u Chat Consumeru za autentifikaciju korisnika

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -2,50 +2,54 @@ import json
 from urllib.parse import parse_qs
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
-from django.contrib.contenttypes.models import ContentType
-from .models import Message
-from accounts.models import ClientToken, BusinessSubjectToken
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from accounts.models import ClientToken, BusinessSubjectToken
+from .models import Message
 
-PAGE_SIZE = 10  
-MAX_BULK_DELETE = 20  
+PAGE_SIZE = 10
+MAX_BULK_DELETE = 20
 
 User = get_user_model()
+online_users = set()
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         query_string = parse_qs(self.scope["query_string"].decode())
         token = query_string.get("token", [None])[0]
 
-        user = await self.authenticate_user(token)
-        if user is None:
+        self.user = await self.authenticate_user(token)
+        if self.user is None:
             await self.close()
             return
 
-        self.scope["user"] = user
-        self.room_group_name = f"chat_{user.id}"
+        self.username = self.user.username
+        online_users.add(self.username)
+        self.room_group_name = "chatroom"
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
+        await self.broadcast_online_users()
 
     async def authenticate_user(self, token):
         if not token:
             return None
-
         try:
             client_token = await sync_to_async(ClientToken.objects.get)(key=token)
             return client_token.client
         except ClientToken.DoesNotExist:
             pass
-
         try:
             bs_token = await sync_to_async(BusinessSubjectToken.objects.get)(key=token)
             return bs_token.business_subject
         except BusinessSubjectToken.DoesNotExist:
-            return None  
+            return None
 
     async def disconnect(self, close_code):
+        if self.user and self.username in online_users:
+            online_users.remove(self.username)
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.broadcast_online_users()
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -57,17 +61,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.handle_fetch_messages(data)
         elif action == "delete_message":
             await self.handle_soft_delete_message(data)
-        elif action == "bulk_delete_messages":
+        elif action == "bulk_delete":
             await self.handle_bulk_delete_messages(data)
         elif action == "edit_message":
             await self.handle_edit_message(data)
 
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps(event))
+
+    async def broadcast_online_users(self):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {"type": "online_users", "users": list(online_users)},
+        )
+
+    async def online_users(self, event):
+        await self.send(text_data=json.dumps(event))
+
     async def handle_send_message(self, data):
-        sender_id = data["sender_id"]
-        sender_type = data["sender_type"]
-        receiver_id = data["receiver_id"]
-        receiver_type = data["receiver_type"]
-        content = data["message"]
+        sender_id = self.user.id
+        sender_type = "user"
+        receiver_id = data.get("receiver_id")
+        receiver_type = data.get("receiver_type")
+        content = data.get("message")
 
         sender_content_type = await sync_to_async(ContentType.objects.get)(model=sender_type)
         receiver_content_type = await sync_to_async(ContentType.objects.get)(model=receiver_type)
@@ -79,7 +95,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             receiver_object_id=receiver_id,
             content=content,
             is_read=False,
-            is_deleted=False  
+            is_deleted=False
         )
 
         await self.channel_layer.group_send(
@@ -94,35 +110,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def handle_fetch_messages(self, data):
-        user = self.scope["user"]
+        user = self.user
         page = int(data.get("page", 1))
-        sender_id = data.get("sender_id")
-        receiver_id = data.get("receiver_id")
-        unread_only = data.get("unread_only", False)
-
         offset = (page - 1) * PAGE_SIZE
 
         query = Message.objects.filter(receiver_object_id=user.id, is_deleted=False)
-
-        if sender_id:
-            query = query.filter(sender_object_id=sender_id)
-        if receiver_id:
-            query = query.filter(receiver_object_id=receiver_id)
-        if unread_only:
-            query = query.filter(is_read=False)
-
         total_messages = await sync_to_async(query.count)()
 
         async def get_and_update_messages():
-            messages = list(query
-                            .select_related("sender_content_type", "receiver_content_type")
-                            .order_by("-timestamp")[offset:offset + PAGE_SIZE])
-
+            messages = list(query.order_by("-timestamp")[offset:offset + PAGE_SIZE])
             Message.objects.filter(id__in=[msg.id for msg in messages]).update(is_read=True)
             return messages
 
         messages = await sync_to_async(get_and_update_messages)()
-
         message_list = [
             {
                 "id": msg.id,
@@ -130,64 +130,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "receiver": msg.receiver_object_id,
                 "message": msg.content,
                 "timestamp": msg.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-                "is_read": True  
+                "is_read": True
             }
             for msg in messages
         ]
 
-        has_more = (offset + PAGE_SIZE) < total_messages  
-
-        await self.send(text_data=json.dumps({
-            "type": "message_history",
-            "messages": message_list,
-            "page": page,
-            "has_more": has_more
-        }))
+        has_more = (offset + PAGE_SIZE) < total_messages
+        await self.send(text_data=json.dumps({"type": "message_history", "messages": message_list, "page": page, "has_more": has_more}))
 
     async def handle_soft_delete_message(self, data):
-        user = self.scope["user"]
         message_id = data.get("message_id")
-
-        if not message_id:
-            return
-
         try:
             message = await sync_to_async(Message.objects.get)(id=message_id)
         except Message.DoesNotExist:
             return
 
-        if message.sender_object_id != user.id:
-            return  
+        if message.sender_object_id != self.user.id:
+            return
 
         message.is_deleted = True
         await sync_to_async(message.save)()
 
-        await self.send(text_data=json.dumps({
-            "type": "message_deleted",
-            "message_id": message_id
-        }))
+        await self.send(text_data=json.dumps({"type": "message_deleted", "message_id": message_id}))
 
     async def handle_bulk_delete_messages(self, data):
-        user = self.scope["user"]
         message_ids = data.get("message_ids", [])
-
         if not message_ids or len(message_ids) > MAX_BULK_DELETE:
-            return  
+            return
 
-        messages = await sync_to_async(list)(Message.objects.filter(id__in=message_ids, sender_object_id=user.id))
-
+        messages = await sync_to_async(list)(Message.objects.filter(id__in=message_ids, sender_object_id=self.user.id))
         for message in messages:
             message.is_deleted = True
 
         await sync_to_async(Message.objects.bulk_update)(messages, ["is_deleted"])
 
-        await self.send(text_data=json.dumps({
-            "type": "bulk_messages_deleted",
-            "message_ids": [msg.id for msg in messages]
-        }))
+        await self.send(text_data=json.dumps({"type": "bulk_messages_deleted", "message_ids": [msg.id for msg in messages]}))
 
     async def handle_edit_message(self, data):
-        user = self.scope["user"]
         message_id = data.get("message_id")
         new_content = data.get("new_content")
 
@@ -199,14 +178,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         except Message.DoesNotExist:
             return
 
-        if message.sender_object_id != user.id:
-            return  
+        if message.sender_object_id != self.user.id:
+            return
 
         message.content = new_content
         await sync_to_async(message.save)()
 
-        await self.send(text_data=json.dumps({
-            "type": "message_edited",
-            "message_id": message_id,
-            "new_content": new_content
-        }))
+        await self.send(text_data=json.dumps({"type": "message_edited", "message_id": message_id, "new_content": new_content}))
--- backend/myproject/asgi.py
+++ backend/myproject/asgi.py
@@ -8,24 +8,26 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from django.urls import re_path  # Update this import
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
-
-from channels.auth import AuthMiddlewareStack   
+from channels.routing import ProtocolTypeRouter, URLRouter
+from accounts.middleware import CustomAuthMiddleware  # Import the custom middleware
 import chat.routing
 import notifications.routing
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns 
-            + notifications.routing.websocket_urlpatterns # Make sure routing is correct
-        )
-    )
-})
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+# Django ASGI application
+django_asgi_app = get_asgi_application()
+
+# WebSocket application with custom authentication middleware
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": CustomAuthMiddleware(  # Replace AuthMiddlewareStack
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+                + notifications.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
