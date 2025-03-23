572a55020b87f4c7d69c01d17ef0d64cd0bac892
sunci0089 <unav660@gmail.com>
2025-03-23 15:21:19 +0100

    provjera da li je korisnik klijent
    webpush radi samo za klijenta

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,5 @@
 //sve funkcije koje se cesto koritste
+import API from "./variables.js";
 import api from "./variables.js"
 import axios from "axios";
 
@@ -16,3 +17,36 @@ export const fetchIdAndTypeOfUser = async () => {
     }
 };
 
+export const fetchCurrentUserData = async () => {
+    const id = await fetchIdAndTypeOfUser();
+    try {
+        const response = await axios.get(`${API}/api/client/${id.id}/`)
+
+        return ({
+            first_name: response.data.first_name,
+            last_name: response.data.last_name,
+            username: response.data.username,
+            email: response.data.email,
+            profile_picture: response.data.profile_picture ? API + response.data.profile_picture : null,
+        })
+    }
+    catch (error) {
+        console.error('Error fetching data: ', error);
+        //alert('Error 404');
+    };
+}
+
+const isUserRegistered = async () => {
+    const id = await fetchIdAndTypeOfUser();
+    if (id.id !== -1)
+        return true;
+    return false;
+}
+
+export const isUserClient = async () => {
+    const id = await fetchIdAndTypeOfUser();
+    if (id.id !== -1 && id.type === "Client")
+        return true;
+    return false;
+
+}
